2096abaf424f318138678502c9b71cc37ae1942c
luistoledolafuente <luismiguel.tlf@gmail.com>
2025-10-15 22:30:49 -0500

    agregando los viewsets

--- src/gestor/views.py
+++ src/gestor/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Propiedad, Propietario
+from .serializers import PropiedadSerializers, PropietarioSerializer
 
-# Create your views here.
+class PropietarioViewSet(viewsets.ModelViewSet):
+    queryset = Propietario.objects.all()
+    serializer_class = PropietarioSerializer
+
+class PropiedadViewSet(viewsets.ModelViewSet):
+    queryset = Propiedad.objects.all()
+    serializer_class = PropiedadSerializers
+    
+    #Aqui implementamos los filtros
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['dirreccion', 'tipo']
